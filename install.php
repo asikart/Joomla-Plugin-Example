4c89d095fde3b30ce4104b1af54c80a3e2be9b21
Asika <asika32764@gmail.com>
2013-09-08 00:15:28 +0800

    UPDATE header comment

--- install.php
+++ install.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * @package        Asikart.Plugin
+ * @package       Asikart.Plugin
  * @subpackage    folder.plg_example
- * @copyright    Copyright (C) 2012 Asikart.com, Inc. All rights reserved.
- * @license        GNU General Public License version 2 or later; see LICENSE.txt
+ * @copyright     Copyright (C) 2012 Asikart.com, Inc. All rights reserved.
+ * @license       GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 // no direct access
